2ef42ca693ff12e971390432fb53e3851d092255
Sasha <nomail>
2021-12-08 08:49:26 +0100

    Day 8 - Funny

--- j-08/qnd.hs
+++ j-08/qnd.hs
@@ -1,4 +1,6 @@
-
+import Data.Map.Lazy(Map, empty, insertWith, (!))
+import Data.List (intersect)
+import Text.ParserCombinators.Parsec (digit)
 
 main :: IO ()
 main = do
@@ -8,7 +10,7 @@ main = do
   resolve "input1.txt"
   putStrLn "--- DataSet 2 ---"
   resolve "input2.txt"
-  
+
 
 resolve :: FilePath -> IO ()
 resolve nameFile = do
@@ -18,16 +20,58 @@ resolve nameFile = do
   putStr "Part Two : "
   print (part2 content)
 
-readInput :: FilePath -> IO [Int]
+readInput :: FilePath -> IO [String]
 readInput nameFile = do
   content <- readFile nameFile
-  return (map read (splitOn "," content))
+  return (lines content)
 
 splitOn :: String -> String -> [String]
 splitOn "" string = [string]
-splitOn delimiters string = words (map (\c -> if c `elem` delimiters then ' ' else c) string)
+splitOn delimiters string = lines (map (\c -> if c `elem` delimiters then '\n' else c) string)
 
-part1 l = 1
+part1 :: [String] -> Integer
+part1 = sum
+  . map (\cur -> if length cur == 2 || length cur == 3 || length cur == 4 || length cur == 7 then 1 else 0 )
+  . concatMap (words . (last . splitOn "|"))
 
-part2 = part1 
-  
+part2 = sum . map computeNumberDisplayed
+
+computeNumberDisplayed :: String -> Int
+computeNumberDisplayed line = deductNumberFrom decoder digits where
+  decoder = makeDecoder encodedSignals
+  digits = words (last splitRes)
+  encodedSignals = words (head splitRes)
+  splitRes = splitOn "|" line
+
+
+deductNumberFrom :: Map Int [Char] -> [String] -> Int
+deductNumberFrom decoder = foldl (\acc cur -> acc * 10 + decode decoder cur) 0
+
+decode :: Map Int [Char] -> String -> Int
+decode decoder word
+  | length word == 2 = 1
+  | length word == 3 = 7
+  | length word == 4 = 4
+  | length word == 7 = 8
+  | length word == 5 && (length ((decoder ! 7) `intersect` word) == 3) = 3
+  | length word == 5 && (length ((decoder ! 4) `intersect` word) == 2) = 2
+  | length word == 5 = 5
+  | length word == 6 && (length ((decoder ! 1) `intersect` word) == 1) = 6
+  | length word == 6 && (length ((decoder ! 4) `intersect` word) == 4) = 9
+  | length word == 6 = 0
+  | length word == 7 = 8
+  | otherwise = error "Oops : Wrong Input"
+
+makeDecoder :: [[Char]] -> Map Int [Char]
+makeDecoder = foldl completeDecoder empty
+
+completeDecoder :: Map Int [Char] -> [Char] -> Map Int [Char]
+completeDecoder decoder word
+  | length word == 2 = foldl (associateLetterWithNumber 1) decoder word
+  | length word == 3 = foldl (associateLetterWithNumber 7) decoder word
+  | length word == 4 = foldl (associateLetterWithNumber 4) decoder word
+  | length word == 7 = foldl (associateLetterWithNumber 8) decoder word
+  | otherwise = decoder
+
+associateLetterWithNumber :: Int -> Map Int [Char] -> Char -> Map Int [Char]
+associateLetterWithNumber number decoder letter = insertWith (++) number [letter] decoder
